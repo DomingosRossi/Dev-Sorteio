85cb7fd084504afd04a31724334507e120479525
DomingosRossi <domingos_neto@msn.com>
2024-04-15 21:41:22 -0300

    add result na tela

--- scripts.js
+++ scripts.js
@@ -2,14 +2,17 @@ function generateNumber() {
 
 const min = Math.ceil(document.querySelector(".input-min").value)
 const max = Math.floor(document.querySelector(".input-max").value)
+let p = document.querySelector("p")
 
 if ( min > max) {
     alert ("Não pode o valor mínimo tem que ser maior que o valor máximo!")
 }
 
 else {
-const result = Math.floor(Math.random() * (max - min + 1)) + min;
+const result = Math.floor(Math.random() * (max - min + 1)) + min; {
+    p.innerHTML = `${result}`
+}
 
-alert(result)
+
 }
 }
